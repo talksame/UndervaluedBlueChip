a1d9a635e9d0f2406c6be0d5a9fb41a7677e9b38
talksame <rnjsdndud4@gmail.com>
2021-05-04 15:38:07 +0900

    Feature : obstalce

---
+++ Undervalued Blue Chip/Assets/Obstacle2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Color color;
+    private SpriteRenderer spriteRenderer;
+    private int cnt = 0;
+    private Vector3 moveDirection = Vector3.zero;
+    private int moveSpeed = 0;
+
+
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        moveDirection = new Vector3(-1, 0 , 0);
+        moveSpeed = Random.Range(13, 20);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (cnt == 1)
+        {
+            Debug.Log("finish");
+            Debug.Log(moveSpeed);
+            //player.GetComponent<SpriteRenderer>().color = color;
+        }
+        else
+        {
+            cnt += 1;
+        }
+    }
+}
