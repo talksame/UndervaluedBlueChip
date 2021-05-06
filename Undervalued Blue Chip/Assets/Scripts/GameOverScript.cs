412028e5d0bdd02b067a8847aac1079f54531caf
Eunyoung <eeeyounglee@gmail.com>
2021-05-06 23:08:23 +0900

    UI files

---
+++ Undervalued Blue Chip/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+}
