7f85e9970259455945945344e298c5af1b78cde6
talksame <rnjsdndud4@gmail.com>
2021-05-08 16:43:47 +0900

    Feature : UI

---
+++ Undervalued Blue Chip/Assets/Scripts/SceneChangeLevel1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeLevel1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LevelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+}
---
+++ Undervalued Blue Chip/Assets/Scripts/SequenceAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    public float WaitBetween = 0;
+    List<Animator> _animators;
+    void Start()
+    {
+        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        while (true)
+        {
+            foreach(var animator in _animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(WaitBetween);
+            }
+        }
+    }
+}
