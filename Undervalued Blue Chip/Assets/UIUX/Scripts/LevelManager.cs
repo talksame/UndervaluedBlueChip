21dacc51680c80c41596b1b1921f1a791824b1d0
Eunyoung <eeeyounglee@gmail.com>
2021-05-28 23:16:46 +0900

    level change update

---
+++ Undervalued Blue Chip/Assets/UIUX/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int levelsUnlocked;
+    public Button[] buttons;
+
+    void Start()
+    {
+        levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 1);
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < levelsUnlocked; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Undervalued Blue Chip/Assets/UIUX/Scripts/LevelScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Pass()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        if (currentLevel >= PlayerPrefs.GetInt("levelsUnlocked"))
+        {
+            PlayerPrefs.SetInt("levelsUnlocked", currentLevel + 1);
+        }
+    }
+}
