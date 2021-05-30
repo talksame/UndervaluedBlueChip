f854293b56685015164d15c5744636f3d2ecb2e1
Eunyoung <eeeyounglee@gmail.com>
2021-05-30 10:49:53 +0900

    level one fixed

---
+++ Undervalued Blue Chip/Assets/UIUX/Scripts/RestartInfinite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartInfinite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LevelInfinite()
+    {
+        SceneManager.LoadScene("LevelInfinite");
+    }
+}
---
+++ Undervalued Blue Chip/Assets/UIUX/Scripts/RestartLv3.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLv3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("LevelThree");
+    }
+}
