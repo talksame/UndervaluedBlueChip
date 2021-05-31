2e18c3a1d4b4f7b25eab9209dc84ef45c682fbe6
talksame <rnjsdndud4@gmail.com>
2021-06-01 02:35:12 +0900

    Add : isSucceed, Score, Sizebar

---
+++ Undervalued Blue Chip/Assets/Wooyoung/Obstacle/IsSucceed.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsSucceed : MonoBehaviour
+{
+    [SerializeField]
+    public IsTriggered isTriggered;
+    public bool isSucceed = false;
+
+    [SerializeField]
+    private float successTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = 0;
+        currentTime += Time.deltaTime;
+        if (successTime < currentTime )
+        {
+            if ( isTriggered.get_die() == false)
+            {
+                isSucceed = true;
+            }
+        }
+        //Debug.Log(obstacleSpawnTime);
+
+
+    }
+
+    public bool get_suc()
+    {
+        return isSucceed;
+    }
+}
