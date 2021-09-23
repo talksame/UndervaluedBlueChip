6b877f217ea288bb8deb9bc5b425f8792c5b8938
Kwon_wooyoung <56039932+talksame@users.noreply.github.com>
2021-09-23 23:59:40 +0900

    Fix Error In INF

--- Undervalued Blue Chip/Assets/Mingyo/PinchZoom.cs
+++ Undervalued Blue Chip/Assets/Mingyo/PinchZoom.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PinchZoom : MonoBehaviour
 {
-    float ZOOM_MAX_X = 1.4f;
-    float ZOOM_MIN_X = 0.7f;
-    float zoomSpeed = 0.1f;
+    [SerializeField]
+    float max_zoom;
+
+    [SerializeField]
+    float min_zoom;
+
+    [SerializeField]
+    float speed;
+    
+    private Vector3 zoomScale;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +22,27 @@ public class PinchZoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //>> 코루틴으로 개선X
+        //>> 매 프레임 단위로 계산해서 반응을 빠르게 해아함.
         OnPinch();
     }
 
     private void OnPinch()
     {
-        if (Input.touchCount != 2)
+        //현재 손가락 터치의 개수가 2개가 이하일경우에는 핀치를 하지 않는다.
+        if (Input.touchCount < 2)
         {
             return;
         }
 
         float scaleX = transform.localScale.x;
         float scaleY = transform.localScale.y;
-        Vector3 zoomScale;
-
+        
+        // 2개의 손가락 좌표를 받음
         Touch firstTouch = Input.GetTouch(0);
         Touch secondTouch = Input.GetTouch(1);
+
+        // 이전 위치와 이후 위치가 달라진다면
 
         Vector2 firstTouchPreviousPosition = firstTouch.position - firstTouch.deltaPosition;
         Vector2 SecondTouchPreviousPosition = secondTouch.position - secondTouch.deltaPosition;
@@ -39,15 +51,15 @@ public class PinchZoom : MonoBehaviour
         float currentTouchDistance = (firstTouch.position - secondTouch.position).magnitude;
 
 
-        float zoomPower = (firstTouch.deltaPosition - secondTouch.deltaPosition).magnitude * zoomSpeed * Time.deltaTime;
+        float zoomPower = (firstTouch.deltaPosition - secondTouch.deltaPosition).magnitude * speed * Time.deltaTime;
 
         if (previousTouchDistance <= currentTouchDistance)
         {
             zoomScale = new Vector3(1, 0, 0) * zoomPower;
 
-            if (scaleX + zoomScale.x >= ZOOM_MAX_X)
+            if (scaleX + zoomScale.x >= max_zoom)
             {
-                transform.localScale = new Vector3(ZOOM_MAX_X, scaleY, 0);
+                transform.localScale = new Vector3(max_zoom, scaleY, 0);
             }
             else
             {
@@ -58,9 +70,9 @@ public class PinchZoom : MonoBehaviour
         {
             zoomScale = new Vector3(-1, 0, 0) * zoomPower;
 
-            if (scaleX + zoomScale.x <= ZOOM_MIN_X)
+            if (scaleX + zoomScale.x <= min_zoom)
             {
-                transform.localScale = new Vector3(ZOOM_MIN_X, scaleY, 0);
+                transform.localScale = new Vector3(min_zoom, scaleY, 0);
             }
             else
             {
